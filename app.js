8e700339913d14ef8fa03b52720ca9e6bee7946c
André Giampaolo <andregiampaolo@gmail.com>
2019-08-22 20:56:23 -0300

    Import mongoose and connect to db

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 require('dotenv').config();
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    { useNewUrlParser: true },
+    () => console.log('Connect to db')
+);
+
 
 app.listen(3000);
