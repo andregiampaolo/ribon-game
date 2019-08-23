e80e36c7a3894a0e6d9f731f426a8856a0abce73
André Giampaolo <andregiampaolo@gmail.com>
2019-08-23 13:34:50 -0300

    Add new route to list users

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = express.Router();
 const UserController = require('../controllers/user-controller');
 
+routes.get('/list', UserController.list);
 routes.post('/register', UserController.register);
 
 module.exports = routes;
