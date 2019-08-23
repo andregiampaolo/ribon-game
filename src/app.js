f740456e542c3d8a6a8fe7e160699dd875bd1824
André Giampaolo <andregiampaolo@gmail.com>
2019-08-22 21:42:41 -0300

    Allow app decode params from url

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
 
 mongoose.connect(
     process.env.DB_CONNECTION,
