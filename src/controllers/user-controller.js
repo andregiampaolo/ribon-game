4e75ebeff67d031cab75b83e737004626d1eea29
André Giampaolo <andregiampaolo@gmail.com>
2019-08-23 13:47:36 -0300

    Create new function to authenticate user

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const bcryptjs = require('bcryptjs');
 
 module.exports = {
     async register(req, res){
@@ -21,6 +22,27 @@ module.exports = {
             return res.json(users);
         } catch (error) {
             return res.status(400).send({error: 'List failed'});
+        }
+    },
+    async authenticate(req, res){
+        const { email, password } = req.body;
+        try {
+
+            const user = await User.findOne({email}).select('+password');
+
+            if(!user){
+                return res.status(400).send({error: 'User or password invalid'});
+            }
+
+            if(! await bcryptjs.compare(password, user.password)){
+                return res.status(400).send({error: 'User or password invalid'});
+            }
+
+            user.password = undefined;
+            return res.send(user);
+
+        } catch (error) {
+            return res.status(400).send({error: 'Authenticate failed'});
         }
     }
 };
