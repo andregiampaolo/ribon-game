d8f46414704b844961beb5dca3d806b593885d88
André Giampaolo <andregiampaolo@gmail.com>
2019-08-23 00:15:41 -0300

    Remove database connection from app.js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,11 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
 require('dotenv').config();
 const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
-
-mongoose.connect(
-    process.env.DB_CONNECTION,
-    { useNewUrlParser: true },
-    () => console.log('Connect to db')
-);
 
 app.listen(port, () => console.log(`Server is running on port ${port}.`));
