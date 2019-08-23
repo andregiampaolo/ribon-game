eecef442030d80055ee97b5ddb79189d724193e3
André Giampaolo <andregiampaolo@gmail.com>
2019-08-23 13:47:54 -0300

    Create new route to authenticate user

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,5 +4,6 @@ const UserController = require('../controllers/user-controller');
 
 routes.get('/list', UserController.list);
 routes.post('/register', UserController.register);
+routes.post('/authenticate', UserController.authenticate);
 
 module.exports = routes;
