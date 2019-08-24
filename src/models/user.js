e421989c8ff326ae2ce529f171e3f4010afd110c
André Giampaolo <andregiampaolo@gmail.com>
2019-08-24 15:35:14 -0300

    Fix attribute required on field name

--- src/models/user.js
+++ src/models/user.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcryptjs');
 const UserSchema = new mongoose.Schema({
     name:{
         type: String,
-        require: true,
+        required: true,
     },
     email:{
         type: String,
