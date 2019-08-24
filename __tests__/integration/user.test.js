24233c4a61036f2155d0feb1b89c248f18516353
André Giampaolo <andregiampaolo@gmail.com>
2019-08-24 15:26:11 -0300

    Create some tests to verify user module

---
+++ __tests__/integration/user.test.js
@@ -0,0 +1,45 @@
+const User = require('../../src/models/user');
+const request = require('supertest');
+const app = require('../../src/app');
+
+describe('User', () => {
+
+    beforeEach(async () => {
+        await Promise.all([
+            User.deleteMany({})
+        ]);
+    });
+
+    it('Register new user', async () => {
+
+        const user = { "name": "Usuario 1", "email": "usuario1@teste.com", "password": "123456" };
+        const response = await request(app)
+            .post('/user/register')
+            .send(user);
+        expect(response.status).toBe(200);
+    });
+
+    it('Duplicated email user', async () => {
+
+        const user = { "name": "Usuario 1", "email": "usuario1@teste.com", "password": "123456" };
+        const response = await request(app)
+            .post('/user/register')
+            .send(user);
+
+        const user2 = { "name": "Usuario 1", "email": "usuario1@teste.com", "password": "123456" };
+        const response2 = await request(app)
+            .post('/user/register')
+            .send(user2);
+
+        expect(response2.status).toBe(400);
+    });
+
+    it('Register new user without email', async () => {
+
+        const user = { "name": "Usuario 1", "password": "123456" };
+        const response = await request(app)
+            .post('/user/register')
+            .send(user);
+        expect(response.status).toBe(400);
+    });
+});
