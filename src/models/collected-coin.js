8bd962a7984c34afac4b064bcc2fed5ca43426d8
André Giampaolo <andregiampaolo@gmail.com>
2019-08-25 15:09:12 -0300

    Create new model of collected-coin

---
+++ src/models/collected-coin.js
@@ -0,0 +1,19 @@
+const mongoose = require('../database');
+
+const CollectedCoinSchema = new mongoose.Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    value:{
+        type: Number,
+        required: true
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    }
+});
+const CollectedCoin = mongoose.model('CollectedCoin', CollectedCoinSchema);
+module.exports = CollectedCoin;
