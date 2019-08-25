56bf1a2ff7515804ba5e0dc3efc1ad1c93cdbbf1
André Giampaolo <andregiampaolo@gmail.com>
2019-08-25 15:14:33 -0300

    Add some returns declarations. Remove call function next

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -6,29 +6,26 @@ module.exports = (req, res, next) => {
     const authHeader = req.headers.authorization;
 
     if(!authHeader){
-        res.status(401).send({ error: 'No token provider' });
+        return res.status(401).send({ error: 'No token provider' });
     }
 
     const parts = authHeader.split(' ');
 
     if(!parts.length === 2){
-        res.status(401).send({ error: 'Token error'});
+        return res.status(401).send({ error: 'Token error'});
     }
 
     const [scheme, token ] = parts;
 
     if(!/^Bearer$/i.test(scheme)){
-        res.status(401).send({ error: 'Token malformatted' });
+        return res.status(401).send({ error: 'Token malformatted' });
     }
 
     jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
         if(err){
-            res.status(401).send({error: 'Token invalid'});
+            return res.status(401).send({error: 'Token invalid'});
         }
-
         req.userId = decoded.id
         return next();
     });
-    
-    next();
 };
