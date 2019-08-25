1e65bf18b15357ec79fac7580850658006fa04a5
André Giampaolo <andregiampaolo@gmail.com>
2019-08-25 17:32:42 -0300

    Create a model to save killed monster

---
+++ src/models/killed-monster.js
@@ -0,0 +1,20 @@
+const mongoose = require('../database');
+
+const KilledMonsterSchema = new mongoose.Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    monster:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Monster',
+        required: true
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('KilledMonster', KilledMonsterSchema);
