6a4cf377e69d4c8c1ee02db8092217c252307849
André Giampaolo <andregiampaolo@gmail.com>
2019-08-25 15:16:20 -0300

    Fix log of seed

--- src/seeders/monster-seed.js
+++ src/seeders/monster-seed.js
@@ -3,7 +3,9 @@ const Monster = require('../models/monster');
 try {
     const monsters = [{"name":"Loki"},{"name":"Coringa"},{"name":"Hannibal Lecter" }];
     monsters.map(monster => Monster.create(monster));
+    console.log('Monsters created');
+    return true;
 } catch (error) {
-    console.log('Monsters created');    
+    console.log('Error: ', error);
+    return false;
 }
-
