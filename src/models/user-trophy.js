fff61a662bffadc5115b17d22097110c64996037
André Giampaolo <andregiampaolo@gmail.com>
2019-08-26 09:39:54 -0300

    Create a model to represents trophies the user has won

---
+++ src/models/user-trophy.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const UserTrophySchema = new mongoose.Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    trophy:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Trophy',
+        required: true
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('UserTrophy', UserTrophySchema);
