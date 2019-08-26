b91c553e60163b1364f1ca3b58022331525b7ce7
André Giampaolo <andregiampaolo@gmail.com>
2019-08-26 09:55:43 -0300

    Create a new mongoose connection and disconnection

---
+++ src/database/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config({
+    path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
+});
+
+const mongoose = require('mongoose');
+
+module.exports = {
+    mongoose,
+    connect: () => {
+        mongoose.Promise = Promise;
+        mongoose.connect(
+            process.env.DB_CONNECTION,
+            { useNewUrlParser: true, useCreateIndex: true }
+        );
+    },
+    disconnect: (done) => {
+        mongoose.disconnect(done);
+    },
+};
