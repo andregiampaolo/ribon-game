a424e0dbff1423e376f372bc4dd21c24a770c513
André Giampaolo <andre.giampaolo@faroseducacional.com.br>
2019-08-30 15:47:35 -0300

    Add new route to register user

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
+import Register from './pages/Register';
 import Game from './pages/Game';
 
 
@@ -9,6 +10,7 @@ export default function Routes(){
     return(
         <BrowserRouter>
             <Route path="/" exact component={Login} />
+            <Route path="/register" component={Register} />
             <Route path="/game" component={Game} />
         </BrowserRouter>
     )
