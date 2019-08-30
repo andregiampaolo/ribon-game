afdf843d743e5c91c431599b1b08d8b31ffcd437
André Giampaolo <andre.giampaolo@faroseducacional.com.br>
2019-08-30 11:36:37 -0300

    Add logo into frontpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import logo from './assets/logo.svg'
+
 function App() {
   return (
     <div className="App">
-
+      <img src={logo} alt="Ribon game" />
     </div>
   );
 }
