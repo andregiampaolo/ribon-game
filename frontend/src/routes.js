74b7440e295a7f588df26e02216e2701d62b3edc
André Giampaolo <andre.giampaolo@faroseducacional.com.br>
2019-08-30 12:28:59 -0300

    Add new route of game page

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
+import Game from './pages/Game';
+
 
 export default function Routes(){
     return(
         <BrowserRouter>
-            <Route path="/" component={Login} />
+            <Route path="/" exact component={Login} />
+            <Route path="/game" component={Game} />
         </BrowserRouter>
     )
 }
