4d53e2495bb468ee0a4134e2a6bd82b4f9be0b51
André Giampaolo <andre.giampaolo@faroseducacional.com.br>
2019-08-30 17:43:31 -0300

    WIP: Get token param and show on body

--- frontend/src/pages/Game.js
+++ frontend/src/pages/Game.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-export default function Game(){
-    return <h1>Board do game!</h1>
+export default function Game( { match } ){
+    return (
+        <div>
+            <h1>Board do game!</h1>
+            <p>token: {match.params.token}</p>
+        </div>
+    );
 }
