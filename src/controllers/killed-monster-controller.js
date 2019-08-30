0d682f55165596017491daa82b7e323a30a4f9f7
André Giampaolo <andregiampaolo@gmail.com>
2019-08-29 22:28:51 -0300

    Create logic to update user and user-trohpy entities when monster is killed

--- src/controllers/killed-monster-controller.js
+++ src/controllers/killed-monster-controller.js
@@ -1,4 +1,5 @@
 const KilledMonster = require('../models/killed-monster');
+const UserTrohpyService = require('../services/user-trophy-service');
 
 module.exports = {
     async killed(req, res){
@@ -6,7 +7,17 @@ module.exports = {
             const userId = req.userId;
             const {monsterId} = req.body;
 
+
             const killed = await KilledMonster.create({user: userId, monster: monsterId});
+            const fieldIncrease = 'totalKilledMonster';
+            const valueIncrease = 1;
+            const user = await UserTrohpyService.increaseUserTotalField(fieldIncrease, userId, valueIncrease);
+            const trophy = await UserTrohpyService.getTrophyEarnedByValue('killed_monster', user.totalKilledMonster);
+            const userHasTrophy = await UserTrohpyService.userHasTrophy(user, trophy);
+
+            if(!userHasTrophy)
+                await UserTrohpyService.giveTrohpyToUser(userId, trophy);
+
             return res.send(killed);
             
         }catch(err){
