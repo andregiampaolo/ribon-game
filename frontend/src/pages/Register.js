e3349fe5b4b2de772bd2c710ea7191ef4ffec3ee
André Giampaolo <andre.giampaolo@faroseducacional.com.br>
2019-08-30 17:58:54 -0300

    Change url

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -24,7 +24,7 @@ export default function Register( { history } ){
             });
 
             const { token } = response.data;
-            history.push(`/game/${token}`);
+            history.push(`/dashboard/${token}`);
 
         } catch (error) {
             console.log(error);
