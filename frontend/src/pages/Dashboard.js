5f2b704e40fce085443cdc2a3b5c5620dfcbe0cf
André Giampaolo <andre.giampaolo@faroseducacional.com.br>
2019-08-30 18:49:00 -0300

    Create a route to start game

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 export default function Dashboard( { match } ){
     return (
-        <div>
+        <div className="dashboard-container">
             <h1>Dashboard do game!</h1>
             <p>token: {match.params.token}</p>
+            <br/>
+            <a href="/game">Start game</a>
         </div>
     );
 }
