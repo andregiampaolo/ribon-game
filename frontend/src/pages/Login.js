a1b62986492697a19553fa9b591d60ae6d64cd8c
André Giampaolo <andre.giampaolo@faroseducacional.com.br>
2019-08-30 15:29:33 -0300

    Create functions to handle form inputs

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 
 import logo from '../assets/logo.svg'
 
 export default function Login(){
+
+    const [form, setValues] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(form.email, form.password);
+    }
+
+    const updateField = e => {
+        setValues({
+          ...form,
+          [e.target.name]: e.target.value
+        });
+      };
+
     return (
         
         <div className="login-container">
             <img src={logo} alt="Ribon game" className="login-logo" />
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input
                     type="text"
                     placeholder="Digite seu email"
+                    name="email"
+                    value={form.email}
+                    onChange={updateField}
                 />
                 <input
-                    type="password"
+                    type="text"
                     placeholder="Digite sua senha"
+                    name="password"
+                    value={form.password}
+                    onChange={updateField}
                 />
                 <button type="submit">Jogar</button>
             </form>
