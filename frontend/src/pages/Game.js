d204a484daa0d34c43067f1c8feae7e7c16ba9ef
André Giampaolo <andre.giampaolo@faroseducacional.com.br>
2019-08-30 18:45:19 -0300

    WIP: Create a component whe user start a game. This will be a container with board, scores and others components

---
+++ frontend/src/pages/Game.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Board from './Board';
+
+
+export default function Game(){
+
+    return(
+        <div>
+            <h1>Estou no game!</h1>
+            <Board />
+        </div>
+    )
+}
