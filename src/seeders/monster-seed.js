bae5d7d3907091b639720175ae7ed63dac0651ea
André Giampaolo <andregiampaolo@gmail.com>
2019-09-02 18:49:31 -0300

    Fix seed to finish process

--- src/seeders/monster-seed.js
+++ src/seeders/monster-seed.js
@@ -5,10 +5,10 @@ mongoose.connect();
 
 try {
     const monsters = [{"name":"Loki"},{"name":"Coringa"},{"name":"Hannibal Lecter" }];
-    monsters.map(monster => Monster.create(monster));
+    monsters.map(async monster => await Monster.create(monster));
     console.log('Monsters created');
-    return true;
 } catch (error) {
     console.log('Error: ', error);
-    return false;
 }
+
+process.exit();
