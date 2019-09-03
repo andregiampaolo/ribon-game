1a07d1021531f6912e9741bffd1d224ff81e75c8
André Giampaolo <andre.giampaolo@faroseducacional.com.br>
2019-09-03 17:28:30 -0300

    Fix seed, logic does not work good. Dont finish process by example. Does not connect to mongodb

--- src/seeders/monster-seed.js
+++ src/seeders/monster-seed.js
@@ -1,14 +1,39 @@
 const Monster = require('../models/monster');
-const mongoose = require('../database');
+const mongoose = require('mongoose');
 
-mongoose.connect();
-
-try {
-    const monsters = [{"name":"Loki"},{"name":"Coringa"},{"name":"Hannibal Lecter" }];
-    monsters.map(monster => Monster.create(monster));
-    console.log('Monsters created');
-    return true;
-} catch (error) {
-    console.log('Error: ', error);
-    return false;
+const connectDb = async () => {
+    return await mongoose.connect(
+        'mongodb://localhost:27017/ribon-game-dev',
+        {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+        },
+        () => {
+            return true;
+        }
+    ).catch(err => {
+        console.log(err);
+        return false;
+    });
 }
+const monsters = [{"name":"Loki"},{"name":"Coringa"},{"name":"Hannibal Lecter" }];
+
+const insertData = async () => {
+    return await Promise.all( monsters.map(monster => Monster.create(monster)) );
+}
+
+
+const seed = async () => {
+    const db = await connectDb();
+    if(db){
+        const result = await insertData();
+        if(result){
+            console.log('Monstros criados');
+            process.exit();
+        }
+        console.log('Erro ao criar os monstros');
+    }
+}
+
+seed();
