88a9873dc44ab84b367395ac33e335a771c512aa
André Giampaolo <andre.giampaolo@faroseducacional.com.br>
2019-09-03 18:05:17 -0300

    Create button to start game

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
-export default function Dashboard( { match } ){
+export default function Dashboard( { match, history } ){
+
+    const startGame = async e => {
+        console.log('jogar!');
+        history.push(`/game/${match.params.token}`);
+    }
+
     return (
         <div className="dashboard-container">
             <h1>Dashboard do game!</h1>
             <p>token: {match.params.token}</p>
             <br/>
-            <a href="/game">Start game</a>
+            <button type="button" onClick={startGame}>Jogar</button>
         </div>
     );
 }
