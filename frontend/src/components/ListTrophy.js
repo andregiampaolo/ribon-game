74bdfa146918adfe689caf6ee5822fb7bc1e16fb
André Giampaolo <andregiampaolo@gmail.com>
2019-09-03 23:51:44 -0300

    Create a component to list all player trophy

---
+++ frontend/src/components/ListTrophy.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import api from '../services/api';
+
+export default class ListTrophy extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          trophies: [],
+          token: props.token,
+        };
+    }
+
+    componentDidMount(){
+        this.getTrophies();
+    }
+    
+    getTrophies = async () => {
+        console.log('Meu token: ', this.state.token);
+
+        const userTrophies = await api.get('/user-trophy/trophies', 
+            { headers: {'Authorization': `Bearer ${this.state.token}` } }
+        );
+        const trophies = new Array();
+        userTrophies.data.map(userTrophy => {
+            const trophy = {
+                id: userTrophy._id,
+                value: userTrophy.trophy.value, 
+                label: userTrophy.trophy.action
+            };
+            trophies.push(trophy);
+        });
+
+        this.setState({trophies})
+    }
+
+    render (){
+        return (
+            <ul>
+                {this.state.trophies.map( trophy => (
+                    <li key={trophy.id}>
+                        {trophy.label} - {trophy.value}
+                    </li>
+                ))}
+            </ul>
+        );
+    }
+}
