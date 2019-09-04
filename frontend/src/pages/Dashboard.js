970743e6bee06c9c646045d6d4605b7033894d83
André Giampaolo <andregiampaolo@gmail.com>
2019-09-03 22:30:49 -0300

    Improve style of dashboard

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,18 +1,29 @@
 import React from 'react';
 
+import './Dashboard.css';
+import logo from '../assets/logo.svg'
+import trophyIcon from '../assets/trophy.svg'
+
 export default function Dashboard( { match, history } ){
 
     const startGame = async e => {
-        console.log('jogar!');
         history.push(`/game/${match.params.token}`);
+    }
+
+    const logout = async e => {
+        history.push(`/`);
     }
 
     return (
         <div className="dashboard-container">
-            <h1>Dashboard do game!</h1>
-            <p>token: {match.params.token}</p>
-            <br/>
-            <button type="button" onClick={startGame}>Jogar</button>
+            <img src={logo} alt="Ribon game" className="logo" />
+                <div className="block-container">
+                    <button type="button" onClick={startGame}>Jogar</button>
+                    <button type="button" onClick={logout}>Logout</button>
+                </div>
+                <div className="trophies-container">
+                    <img src={trophyIcon} alt="Ribon game" className="logo" />
+                </div>
         </div>
     );
 }
