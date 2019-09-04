a13c21d8936d0e53b83b2b00ea1213edf18009f1
André Giampaolo <andregiampaolo@gmail.com>
2019-09-03 22:02:45 -0300

    Add possibility to die and kill a monster

--- frontend/src/pages/Game.js
+++ frontend/src/pages/Game.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import CollectCoin from '../components/game/CollectCoin';
+import Die from '../components/game/Die';
+import KillMonster from '../components/game/KillMonster';
 
-export default function Game( { match }){
+
+import '../components/game/GameComponents.css';
+import logo from '../assets/logo.svg'
+
+export default function Game( { match, history }){
     
+    const backToDashboard = async e => {
+        history.push(`/dashboard/${match.params.token}`);
+    }
+
     return(
-        <div>
-            <CollectCoin token={match.params.token}/>
+        <div className="container">
+            <img src={logo} alt="Ribon game" className="logo" />
+            <div className="block-container">
+                <CollectCoin token={match.params.token}/>
+                <Die token={match.params.token}/>
+                <KillMonster token={match.params.token}/>
+            </div>
+            <button onClick={backToDashboard} className="button-back" >Voltar</button>
         </div>
     )
 }
