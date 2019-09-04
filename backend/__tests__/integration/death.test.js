16927529948015a5393a95c2a6022eb6eed8181c
André Giampaolo <andregiampaolo@gmail.com>
2019-09-04 14:04:25 -0300

    Create teste to verify increase total value in user and user trophy

--- backend/__tests__/integration/death.test.js
+++ backend/__tests__/integration/death.test.js
@@ -3,6 +3,7 @@ const app = require('../../src/app');
 const Death = require('../../src/models/death');
 const User = require('../../src/models/user');
 const Trophy = require('../../src/models/trophy');
+const UserTrophy = require('../../src/models/user-trophy');
 const mongoose = require('mongoose');
 
 describe('Death', () => {
@@ -13,18 +14,43 @@ describe('Death', () => {
 
     beforeEach(async () => {
         await Promise.all([
-            User.deleteMany({})
+            User.deleteMany({}),
+            Trophy.create({'action' : 'death', 'value': 1})
         ]);
-        await Trophy.create({'action' : 'death', 'value': 1});
     });
+    
+    it('Test die', async (user) => {
 
-    it('Test die on api', async () => {
         const user = await User.create({ "name": "Usuario 1", "email": "usuario1@teste.com", "password": "123456" });
         const token = await User.generateToken({ id: user.id });
         const response = await request(app)
             .post('/death/die')
             .set('Authorization', 'Bearer ' + token);
         expect(response.status).toBe(200);
+    });
+
+    it('Test increase total deaths', async () => {
+        const userBefore = await User.create({ "name": "Usuario 1", "email": "usuario1@teste.com", "password": "123456" });
+        const totalDeathsBefore = userBefore.totalDeaths;
+        const token = await User.generateToken({ id: userBefore.id });
+        await request(app)
+            .post('/death/die')
+            .set('Authorization', 'Bearer ' + token);
+        const userAfter = await User.findById(userBefore._id);
+        const totalDeathsAfter = userAfter.totalDeaths;
+        expect(totalDeathsBefore).toBe((totalDeathsAfter-1));
+    });
+
+    it('Test death trophy is given to the user when they die', async () => {
+        const userBefore = await User.create({ "name": "Usuario 1", "email": "usuario1@teste.com", "password": "123456" });
+        const quantityUserTrophiesBefore = await UserTrophy.countDocuments({user: userBefore._id});
+        const authorizationToken = await User.generateToken({ id: userBefore.id });
+        await request(app)
+            .post('/death/die')
+            .set('Authorization', 'Bearer ' + authorizationToken);
+
+        const quantityUserTrophiesAfter = await UserTrophy.countDocuments({user: userBefore._id});
+        expect(quantityUserTrophiesBefore).toBe((quantityUserTrophiesAfter-1));
     });
 
     it('Test die on database', async () => {
