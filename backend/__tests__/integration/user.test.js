78340c8b47c8532d872248e1aaffc68eaa9bc7c3
André Giampaolo <andregiampaolo@gmail.com>
2019-09-05 04:46:37 -0300

    Make more tests to verify integrity of code

--- backend/__tests__/integration/user.test.js
+++ backend/__tests__/integration/user.test.js
@@ -3,7 +3,10 @@ const request = require('supertest');
 const app = require('../../src/app');
 const mongoose = require('mongoose');
 
-describe('Registration User', () => {
+let user = { "name": "Usuario 1", "email": "usuario1@teste.com", "password": "123456" };
+
+describe('User', () => {
+
 
     afterAll((done) => {
         mongoose.disconnect(done);
@@ -16,8 +19,6 @@ describe('Registration User', () => {
     });
 
     it('Register new user', async () => {
-
-        const user = { "name": "Usuario 1", "email": "usuario1@teste.com", "password": "123456" };
         const response = await request(app)
             .post('/user/register')
             .send(user);
@@ -26,8 +27,7 @@ describe('Registration User', () => {
 
     it('Duplicated email user', async () => {
 
-        const user = { "name": "Usuario 1", "email": "usuario1@teste.com", "password": "123456" };
-        const response = await request(app)
+        await request(app)
             .post('/user/register')
             .send(user);
 
@@ -39,43 +39,100 @@ describe('Registration User', () => {
         expect(response2.status).toBe(400);
     });
 
-    it('Register new user without email', async () => {
+    it('Number of users on database increase when create new user', async () => {
+        const quantityUsersBeforeCreate = await User.countDocuments({});
+        await User.create(user);
+        const quantityUsersAfterCreate = await User.countDocuments({});
+        expect(quantityUsersBeforeCreate).toBe(quantityUsersAfterCreate-1);
+    });
 
-        const user = { "name": "Usuario 1", "password": "123456" };
+    it('Register new user without email', async () => {
         const response = await request(app)
             .post('/user/register')
-            .send(user);
+            .send({ "name": "Usuario 1", "password": "123456" });
         expect(response.status).toBe(400);
     });
 
     it('Register new user without name', async () => {
-
-        const user = { "email": "usuario1@teste.com", "password": "123456" };
         const response = await request(app)
             .post('/user/register')
-            .send(user);
+            .send({ "email": "usuario1@teste.com", "password": "123456" });
         expect(response.status).toBe(400);
     });
     it('Register new user without password', async () => {
-
-        const user = { "name": "Usuario 1", "email": "usuario1@teste.com"};
         const response = await request(app)
             .post('/user/register')
-            .send(user);
+            .send({ "name": "Usuario 1", "email": "usuario1@teste.com" });
         expect(response.status).toBe(400);
     });
 
-    it('Number of users on database increase when create new user', async () => {
-
-        const quantityUsersBeforeCreate = await User.countDocuments({}, (err, quantity) => {
-            return quantity;
-        });
-        await User.create({ "name": "Usuario 1", "email": "usuario1@teste.com", "password": "123456" });
-        const quantityUsersAfterCreate = await User.countDocuments({}, (err, quantity) => {
-            return quantity;
-        });
-        expect(quantityUsersAfterCreate-1).toBe(quantityUsersBeforeCreate);
+    it("Test list users", async () => {
+        const response = await request(app)
+            .get('/user/list');
+        expect(response.status).toBe(200);
     });
 
+    it('Test authenticate user', async () => {
+        await request(app)
+            .post('/user/register')
+            .send(user);
+        
+        const response = await request(app)
+            .post('/user/authenticate')
+            .send(user);
+
+        expect(response.status).toBe(200);
+    });
+
+    it('Test authenticate user with invalid email', async () => {
+        await request(app)
+            .post('/user/register')
+            .send(user);
+
+        const response = await request(app)
+            .post('/user/authenticate')
+            .send({email: 'fake@email.com', password: user.password});
+
+        expect(response.status).toBe(400);
+    });
+
+    it('Test authenticate user without email', async () => {
+
+        await request(app)
+            .post('/user/register')
+            .send(user);
+
+        const response = await request(app)
+            .post('/user/authenticate')
+            .send({email: null, password: user.password});
+
+        expect(response.status).toBe(400);
+    });
+
+    it('Test authenticate user with invalid password', async () => {
+        await request(app)
+            .post('/user/register')
+            .send(user);
+            
+        const response = await request(app)
+            .post('/user/authenticate')
+            .send({email: user.email, password: '123'});
+
+        expect(response.status).toBe(400);
+    });
+
+
+    it('Test authenticate user without password', async () => {
+
+        await request(app)
+            .post('/user/register')
+            .send(user);
+
+        const response = await request(app)
+            .post('/user/authenticate')
+            .send({email: user.email, password: null});
+
+        expect(response.status).toBe(400);
+    });
 
 });
