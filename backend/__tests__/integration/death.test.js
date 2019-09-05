8d53d168853d65f2dbe49243e6f277088ff56563
André Giampaolo <andregiampaolo@gmail.com>
2019-09-05 04:46:06 -0300

    Make more tests to verify integrity of code

--- backend/__tests__/integration/death.test.js
+++ backend/__tests__/integration/death.test.js
@@ -15,24 +15,46 @@ describe('Death', () => {
     beforeEach(async () => {
         await Promise.all([
             User.deleteMany({}),
-            Trophy.create({'action' : 'death', 'value': 1})
+            Death.deleteMany({}),
+            UserTrophy.deleteMany({}),
+            Trophy.deleteMany({}),
         ]);
+        await Trophy.create({'action' : 'death', 'value': 1});
     });
-    
-    it('Test die', async (user) => {
 
+    it('Test if user die', async () => {
         const user = await User.create({ "name": "Usuario 1", "email": "usuario1@teste.com", "password": "123456" });
-        const token = await User.generateToken({ id: user.id });
+        const token = await User.generateAuthorizationToken({ id: user.id });
         const response = await request(app)
             .post('/death/die')
             .set('Authorization', 'Bearer ' + token);
         expect(response.status).toBe(200);
     });
 
-    it('Test increase total deaths', async () => {
+    it('Test if number of deaths increase on database, when user die', async () => {
+
+        const amountDeathBefore = await Death.countDocuments();
+        const user = await User.create({ "name": "Usuario 1", "email": "usuario1@teste.com", "password": "123456" });
+        const token = await User.generateAuthorizationToken({ id: user.id });
+        await request(app)
+            .post('/death/die')
+            .set('Authorization', 'Bearer ' + token);
+
+        const amountDeathAfter = await Death.countDocuments();
+
+        expect(amountDeathBefore).toBe(amountDeathAfter-1);
+    });
+
+    it("Test if user can't die without authorization token", async () => {
+        const response = await request(app)
+            .post('/death/die');
+        expect(response.status).toBe(401);
+    });
+
+    it('Test if total deaths of user increase', async () => {
         const userBefore = await User.create({ "name": "Usuario 1", "email": "usuario1@teste.com", "password": "123456" });
         const totalDeathsBefore = userBefore.totalDeaths;
-        const token = await User.generateToken({ id: userBefore.id });
+        const token = await User.generateAuthorizationToken({ id: userBefore.id });
         await request(app)
             .post('/death/die')
             .set('Authorization', 'Bearer ' + token);
@@ -41,10 +63,10 @@ describe('Death', () => {
         expect(totalDeathsBefore).toBe((totalDeathsAfter-1));
     });
 
-    it('Test death trophy is given to the user when they die', async () => {
+    it('Test if amounth of user death trophy increases', async () => {
         const userBefore = await User.create({ "name": "Usuario 1", "email": "usuario1@teste.com", "password": "123456" });
         const quantityUserTrophiesBefore = await UserTrophy.countDocuments({user: userBefore._id});
-        const authorizationToken = await User.generateToken({ id: userBefore.id });
+        const authorizationToken = await User.generateAuthorizationToken({ id: userBefore.id });
         await request(app)
             .post('/death/die')
             .set('Authorization', 'Bearer ' + authorizationToken);
@@ -53,10 +75,17 @@ describe('Death', () => {
         expect(quantityUserTrophiesBefore).toBe((quantityUserTrophiesAfter-1));
     });
 
-    it('Test die on database', async () => {
+    it("Test if user can't win a death trophy without trophies on database", async () => {
+        await Trophy.deleteMany({});
         const user = await User.create({ "name": "Usuario 1", "email": "usuario1@teste.com", "password": "123456" });
-        const death = await Death.create({user: user.id});
-        expect(typeof death).toBe("object");
+        const token = await User.generateAuthorizationToken({ id: user.id });
+        const response = await request(app)
+            .post('/death/die')
+            .set('Authorization', 'Bearer ' + token);
+        expect(response.status).toBe(400);
     });
 
 });
+
+
+
